01998d316183592e932b01e0ac317144385ba5eb
SWyattGameDesign <wyatts@sheridancollege.ca>
2026-01-13 23:55:33 -0500

    Update CamouflageAT.cs
    Finished making the camouflage so that the lizard turns a different color

--- Week01_Project/Assets/Scripts/CamouflageAT.cs
+++ Week01_Project/Assets/Scripts/CamouflageAT.cs
@@ -7,6 +7,9 @@ namespace NodeCanvas.Tasks.Actions {
 	[Description("An Action Task that changes the GameObject's color")]
 	public class CamouflageAT : ActionTask {
 
+		public GameObject lizard;
+		Color camouflage;
+
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
@@ -17,7 +20,8 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			EndAction(true);
+			camouflage = new Color32((byte)Random.Range(0f, 255f), (byte)Random.Range(0f, 255f), (byte)Random.Range(0f, 255f), (byte)255f);
+			lizard.GetComponent<Renderer>().material.color = camouflage;
 		}
 
 		//Called once per frame while the action is active.
@@ -27,8 +31,9 @@ namespace NodeCanvas.Tasks.Actions {
 
 		//Called when the task is disabled.
 		protected override void OnStop() {
-			
-		}
+			lizard.GetComponent<Renderer>().material.color = new Color32((byte)9f, (byte)217f, (byte)35f, 255);
+            EndAction(true);
+        }
 
 		//Called when the task is paused.
 		protected override void OnPause() {
