4d406e85d048011fe00992688571806f39c75875
SWyattGameDesign <wyatts@sheridancollege.ca>
2026-01-27 16:13:28 -0500

    Break in Week 3 project
    Requiring to take a break

--- Week01_Project/Assets/Scripts/WorkAT.cs
+++ Week01_Project/Assets/Scripts/WorkAT.cs
@@ -1,10 +1,13 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class WorkAT : ActionTask {
+
+		public BBParameter<float> stress;
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -16,12 +19,13 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			EndAction(true);
+			
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			
+
+			stress.value += 5 * Time.deltaTime;
 		}
 
 		//Called when the task is disabled.
